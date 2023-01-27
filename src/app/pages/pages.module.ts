b73ae50bdc2c363542d1e3699b46d5c594864626
Leonardo Leal <117487925+Leozhr@users.noreply.github.com>
2023-01-27 13:52:21 -0300

    Adicionando o Componente do Menu

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { ErrorComponent } from './error/error.component';
+import { MenuComponent } from './home/menu/menu.component';
 
 @NgModule({
-  declarations: [HomeComponent, ErrorComponent],
+  declarations: [HomeComponent, ErrorComponent, MenuComponent],
   imports: [CommonModule],
 })
 export class PagesModule {}
