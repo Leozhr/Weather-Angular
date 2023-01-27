be914db02a7d6e1f6be2dd98641e32ed0ce2fde0
Leonardo Leal <117487925+Leozhr@users.noreply.github.com>
2023-01-27 13:13:10 -0300

    Adicionando Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorComponent } from './components/error/error.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
